74a7f47b2382c0a284b62a65bcd6e8afbbf7b8c0
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-02-08 12:58:35 +0100

    Exo 177 - PHP

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 
+require "./classes/StrUtils.php";
 // TODO n'oubliez pas de créer votre classe dans le dossier classes.
+
+$text = new StrUtils("Yawie Wawie");
+
+echo $text->bold()."<br>";
+echo $text->italic()."<br>";
+echo $text->underline()."<br>";
+echo $text->capitalize()."<br>";
+echo $text->uglify()."<br>";
 
